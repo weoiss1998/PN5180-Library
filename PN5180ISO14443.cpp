532ccfb6a781f94860466a93c3e4a065b20fe62b
weoiss1998 <simon.heinrich.weiss@web.de>
2024-09-02 18:31:41 +0200

    Allow old SPI connection

--- PN5180ISO14443.cpp
+++ PN5180ISO14443.cpp
@@ -27,6 +27,9 @@ PN5180ISO14443::PN5180ISO14443(uint8_t SSpin, uint8_t BUSYpin, uint8_t RSTpin, u
 	: PN5180(SSpin, BUSYpin, RSTpin, SCKpin, MISOpin, MOSIpin, spi)
 {
 }
+PN5180ISO14443::PN5180ISO14443(uint8_t SSpin, uint8_t BUSYpin, uint8_t RSTpin, SPIClass& spi) 
+              : PN5180(SSpin, BUSYpin, RSTpin, spi) {
+}
 
 bool PN5180ISO14443::setupRF()
 {
